8b18761661f2aec51fc626cfcc3818d66846ad36
Toure <tdunnon@gmail.com>
2016-07-14 15:01:41 -0400

    changes to xml generation.

--- linkr.py
+++ linkr.py
@@ -3,11 +3,16 @@ import argparse
 from junit_xml import TestSuite, TestCase
 from sys import argv, exit
 import time
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description="Example: python linkr.py -s rhos-203004 -n 2 -ts rhos-23888 -tc 'rhos-1234"
+                                             ", rhos-24050' -tn undercloud_test -o /tmp/results.xml")
 
-parser.add_argument("-s", "--status", dest="status", help="Test output result, `pass` | `fail` | `incomplete`.")
+parser.add_argument("-s", "--skip_test_name", dest="skip_test", help="Name of test(s) to mark skipped.")
+parser.add_argument("-n", "--number_of_test", default= 1, dest="number_test", metavar="2",
+                    help="Number of test to include in output file, this option will take a number and require a list"
+                         " of test_cases for the `--test_case` option.")
 parser.add_argument("-ts", "--test_suite", dest="ts", help="Test suite id.")
-parser.add_argument("-tc", "--test_case", dest="tc", help="Test Case id")
+parser.add_argument("-tc", "--test_case", dest="tc", help="Test Case id, if number of test cases is `1`, else provide a"
+                                                          " list seperated by commas: rhos-1234, rhos-2345")
 parser.add_argument("-tn", "--test_name", dest="tn", help="Test Case name: (str) ex: Test_Undercloud")
 parser.add_argument("-o", "--output_file", dest="output_f", help="Junit output filename")
 # parser.add_argument("-r", "--requirements.txt", dest="req", help="Test case requirements.txt: "
@@ -18,12 +23,14 @@ args = parser.parse_args()
 
 class Linkr(object):
     def __init__(self):
-        self.status = args.status
+        self.skip = args.skip_test
+        self.number_test = args.number_test
         self.tc = args.tc
         self.tn = args.tn
         self.ts = args.ts
         self.et = time.clock()
         self.output_f = args.output_f
+        # TODO work on requirement upload.
         #self.req = args.req
 
     def gen_junit(self):
@@ -31,8 +38,13 @@ class Linkr(object):
         Generates junit xml file from arguments passed on the cli.
         :return: junit.xml
         """
-        test_case = [TestCase(self.tn, self.tc, self.et, self.status)]
-        ts = TestSuite(self.ts, test_case)
+        if self.number_test == 1:
+            #test_case = [TestCase(self.tn, None, self.tc, self.et)]
+            #test_case.append(TestCase())
+            test_case = [TestCase('Deployment_Test: Undercloud_Test', '', 0.000, '', '')]
+            test_case.append(TestCase('Deployment_Test: Undercloud_Properties', '', 0.01, '', ''))
+
+            ts = TestSuite(self.ts, test_case)
 
         with open(self.output_f, 'w') as results:
             TestSuite.to_file(results, [ts])
