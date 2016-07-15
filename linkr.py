4384ff721bc07e6041ba3ff167db0bf9dd8dae3f
Toure <toure@redhat.com>
2016-07-15 11:09:04 -0400

    refactor.

--- linkr.py
+++ linkr.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 import argparse
-from junit_xml import TestSuite, TestCase
-from xml.etree import ElementTree
+from junitxml import TestSuite, TestCase
 from sys import argv, exit
 
 parser = argparse.ArgumentParser(
     description="Example: python linkr.py -s rhos-203004 -n 2 -ts rhos-23888 -tc 'rhos-1234, "
                 "rhos-24050' -tn undercloud_test -o /tmp/results.xml")
 
-parser.add_argument("-s", "--skip_test_name", dest="skip_test",
+parser.add_argument("-s", "--skip_test_name", nargs='+', dest="skip_test",
                     help="Name of test(s) to mark skipped.")
 
 parser.add_argument("-ts", "--test_suite", dest="ts",
@@ -31,52 +30,39 @@ parser.add_argument("-t", "--tags", default=None, dest="tags",
 
 parser.add_argument("-o", "--output_file", dest="output_f",
                     help="Junit output filename")
-# parser.add_argument("-r", "--requirements.txt", dest="req", help="Test case requirements.txt: "
-#                                                            "`test foo will test this blah functionality`")
 
 args = parser.parse_args()
 
 
-class LinkR(object):
-    @classmethod
-    def gen_junit(cls):
-        """
-        Generates junit xml file from arguments passed on the cli.
-        :return: junit.xml
-        """
-        if args.skip_test:
+def gen_junit():
+    """
+    Generates junit xml file from arguments passed on the cli.
+    :return: junit.xml
+    """
 
-            args.tc.pop()
-        if len(args.tc) == 1:
-            test_case = [TestCase("{}: {}".format(args.tn, args.tc), '', args.et, '', ''),
-                         TestCase("property,{'name': 'polarion-custom-notes', 'value': args.job_url}")]
-            ts = TestSuite(args.ts, test_case)
-        else:
-            test_case = [TestCase("{}: {}".format(args.tn, args.tc.pop(0)), '', args.et, '', '')]
+    if len(args.tc) == 1:
+        test_case = [TestCase("{}: {}".format(args.tn, args.tc.pop(0)), '', args.et, '', '')]
+        ts = TestSuite(args.ts, test_case, properties={'polarion-custom-jenkinsjobs': args.job_url,
+                                                       'polarion-custom-isautomated': True,
+                                                       'polarion-custom-tags': args.tags})
+    else:
+        # TODO complete skip test logic.
+        # if args.skip_test:
+        #    for test in args.skip_test:
+        #        test_case = [TestCase("{}: {}".format(args.tn, test), '', args.et, '', '').add_skipped_info(
+        #            message="this is a test skip message.")]
+        # else:
+        test_case = [TestCase("{}: {}".format(args.tn, args.tc.pop(0)), '', args.et, '', '')]
 
-            for cases in args.tc:
-                test_case.append(TestCase("{}: {}".format(args.tn, cases), '', args.et, '', ''))
+        for cases in args.tc:
+            test_case.append(TestCase("{}: {}".format(args.tn, cases), '', args.et, '', ''))
 
-            ts = TestSuite(args.ts, test_case)
+        ts = TestSuite(args.ts, test_case, properties={'polarion-custom-jenkinsjobs': args.job_url,
+                                                       'polarion-custom-isautomated': True,
+                                                       'polarion-custom-tags': args.tags})
 
-        with open(args.output_f, 'w') as results:
-            TestSuite.to_file(results, [ts])
-
-    @staticmethod
-    def property_inject():
-        """
-        Property_inject will append additional properties needed for polarion.
-        :return: modified results file.
-        """
-        # TODO complete this method.
-        xunit_xml_parser = ElementTree.parse(args.output_f)
-        properties_tag = ElementTree.SubElement(xunit_xml_parser.getroot(), 'testsuites')
-        # Add the Notes property.
-        notes_property = ElementTree.SubElement(properties_tag, 'property', {'name': 'polarion-custom-notes',
-                                                                             'value': args.job_url})
-        # Add the Jenkins Job property.
-        jenkins_job_property = ElementTree.SubElement(properties_tag, 'property', {'name': 'polarion-custom-jenkinsjobs',
-                                                                                   'value': args.job_url})
+    with open(args.output_f, 'w') as results:
+        TestSuite.to_file(results, [ts])
 
 
 def main():
@@ -88,8 +74,7 @@ def main():
         parser.print_help()
         exit(1)
     else:
-        LinkR.gen_junit()
-
+        gen_junit()
 
 if __name__ == "__main__":
     main()
