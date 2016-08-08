a4ed885d5a9e9405e39b590e212aed8ce2386386
Toure <tdunnon@gmail.com>
2016-08-08 14:53:24 -0400

    additional properties added.

--- linkr.py
+++ linkr.py
@@ -43,33 +43,26 @@ def gen_junit():
     if len(args.tc) == 1:
         gen_polarion_property_file(args.tc)
         test_case = [TestCase(args.tc.pop(0), '', args.et, '', '')]
-        ts = [TestSuite(args.ts, test_case, properties={'polarion-project-id': args.project,
-                                                        'polarion-custom-isautomated': True,
-                                                        'polarion-custom-tags': args.tags})]
+        ts = [TestSuite(args.project, test_case, properties={'polarion-project-id': args.ts})]
     else:
         gen_polarion_property_file(args.tc)
         test_case = [TestCase(args.tc.pop(0), '', args.et, '', '')]
         for cases in args.tc:
             test_case.append(TestCase(cases, '', args.et, '', ''))
 
-        ts = [TestSuite(args.ts, test_case, properties={'polarion-project-id': args.project,
-                                                        'polarion-custom-isautomated': True,
-                                                        'polarion-custom-tags': args.tags})]
+        ts = [TestSuite(args.project, test_case, properties={'polarion-project-id': args.ts})]
 
     with open(args.output_f, 'w') as results:
         TestSuite.to_file(results, ts)
 
 
-def gen_polarion_property_file(testcase_id, filename="polarion.properties"):
+def gen_polarion_property_file(testcase_id, filename="polarion.props"):
     """
     Generate a simple mapping file.
     :return: polarion.properties
     """
     with open(filename, 'w') as f:
-        f.write("polarion.project=RHELOpenStackPlatform\n"
-                "polarion.run=LinkR Test\n"
-                "polarion.polarion-id={}\n".format(args.ts)
-                )
+        f.write("polarion.run=LinkR Test\n")
         for tc_id in testcase_id:
             f.write("{}={}\n".format(tc_id, tc_id))
         f.close()
